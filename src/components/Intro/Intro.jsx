0bad9c9d44f2397ad6e6a14df3007bfc2476c1d5
Kristina Bekher <bekherkr@gmail.com>
2023-09-27 23:08:35 +0200

    Add intro section

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -3,11 +3,21 @@ import './Intro.scss';
 export const Intro = () => {
   return (
     <div className="Intro">
-      <h1 className="Intro__title">Ivan Inozemtsev</h1>
-      {/* <p className="Intro__subtitle">
-        Ukrainian designer & illustrator based in Germany. 
-        Please visit my Instagram to keep up with my latest work. 
-      </p> */}
+      <h1 className="visually-hidden">Ivan Inozemtsev</h1>
+
+      <div className="Intro__content">
+        <div className="Intro__holder">
+          <img
+            className="Intro__title-img"
+            src="./images/intro/title.webp"
+            alt="Ivan Inozemtsev"
+          />
+          <p className="Intro__subtitle">
+            Ukrainian designer & illustrator based in Germany.
+            Please visit my Instagram to keep up with my latest work.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };

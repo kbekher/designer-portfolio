de107399d87fc2679ebaf33c503340dec70231d2
Kristina Bekher <bekherkr@gmail.com>
2023-09-30 16:39:25 +0200

    Update Intro.jsx

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -44,14 +44,14 @@ export const Intro = () => {
   const [vh, setVh] = useState(window.innerHeight * 0.01);
 
   useEffect(() => {
-    const handleResize = () => {
+    // const handleResize = () => {
       setVh(window.innerHeight * 0.01);
-    };
+    // };
 
-    window.addEventListener('resize', handleResize);
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
+    // window.addEventListener('resize', handleResize);
+    // return () => {
+    //   window.removeEventListener('resize', handleResize);
+    // };
   }, []);
 
 return (

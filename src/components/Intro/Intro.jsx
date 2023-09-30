87846ff9e24dcc6a0dc1431b7d48854be5e8c8c5
Kristina Bekher <bekherkr@gmail.com>
2023-09-30 12:47:30 +0200

    Try to fix Intro height

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -42,9 +42,10 @@ const transitionBgStyles = {
 export const Intro = () => (
   <TransitionGroup>
     <div className="Intro">
-      <h1 className="visually-hidden">Ivan Inozemtsev</h1>
 
-      <div className="Intro__content">
+      <h1 className="visually-hidden">Ivan Inozemtsev</h1>
+<div className="Intro__inner">
+<div className="Intro__content">
         <div className="Intro__holder">
           <CSSTransition in appear timeout={duration1}>
             {(state) => (
@@ -92,6 +93,8 @@ export const Intro = () => (
           </div>
         )}
       </CSSTransition>
+</div>
+      
     </div>
   </TransitionGroup>
 );

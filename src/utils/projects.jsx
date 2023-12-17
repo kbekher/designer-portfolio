15fb6e54e7c22474903198b76c0858e36c642f8a
Kristina Bekher <bekherkr@gmail.com>
2023-12-17 12:28:22 +0100

    add XML sitemap

--- src/utils/projects.jsx
+++ src/utils/projects.jsx
@@ -1,107 +1,107 @@
 export const projects = [{
   id: 0, 
-  name: "Aura Wine Collective",
+  name: "Ivan Inozemtsev's project Aura Wine Collective",
   imageIds: [ "wine1", "wine2", "wine3", "wine4" ]
 }, {
   id: 1, 
-  name: 'The Holiday Is Coming...',
+  name: "Ivan Inozemtsev's project The Holiday Is Coming...",
   imageId: "grinch"
 }, {
   id: 2, 
-  name: `Breath of the Alps`,
+  name: "Ivan Inozemtsev's project Breath of the Alps",
   imageId: "alps"
 }, {
   id: 3,
-  name: "Divchata Agency",
+  name: "Ivan Inozemtsev's project Divchata Agency",
   imageId: "divchata"
 }, {
   id: 4,
-  name: "Star Porgis",
+  name: "Ivan Inozemtsev's project Star Porgis",
   imageId: "porgis"
 }, {
   id: 5,
-  name: "Which Einstein are you today?",
+  name: "Ivan Inozemtsev's project Which Einstein are you today?",
   imageId: "einstein"
 }, {
   id: 6,
-  name: "Film Speed Lab",
+  name: "Ivan Inozemtsev's project Film Speed Lab",
   imageId: "fsl"
 }, {
   id: 7,
-  name: "Divided Layers",
+  name: "Ivan Inozemtsev's project Divided Layers",
   imageId: "layers"
 }, {
   id: 8,
-  name: "Fragment Design",
+  name: "Ivan Inozemtsev's project Fragment Design",
   imageId: "fragment"
 }, {
   id: 9,
-  name: "Switzerland",
+  name: "Ivan Inozemtsev's project Switzerland",
   imageId: "switzerland"
 }, {
   id: 10,
-  name: "Black and White spots",
+  name: "Ivan Inozemtsev's project Black and White spots",
   imageId: "spots"
 }, {
   id: 11,
-  name: "Each day's potential",
+  name: "Ivan Inozemtsev's project Each day's potential",
   imageId: "potential"
 }, {
   id: 12,
-  name: "b-n Studio",
+  name: "Ivan Inozemtsev's project b-n Studio",
   imageId: "icy"
 }, {
   id: 13,
-  name: "Abstract Layers",
+  name: "Ivan Inozemtsev's project Abstract Layers",
   imageId: "poster"
 }, {
   id: 14,
-  name: "RVTC Rösterei Vier",
+  name: "Ivan Inozemtsev's project RVTC Rösterei Vier",
   imageId: "rvtc"
 }, {
   id: 15,
-  name: "Accidentlly Wes Anderson",
+  name: "Ivan Inozemtsev's project Accidentlly Wes Anderson",
   imageIds: [ "awa1", "awa2", "awa3" ]
 }, 
 {
   id: 16,
-  name: "Tropicfeel",
+  name: "Ivan Inozemtsev's project Tropicfeel",
   subtitle: "Go Beyond Travel",
   imageId: "tropicfeel"
 }, {
   id: 17,
-  name: "Media Development Foundation",
+  name: "Ivan Inozemtsev's project Media Development Foundation",
   subtitle: "Visual identity",
   imageIds: [ "mdf1", "mdf2", "mdf3", "mdf4" ]
 }, {
   id: 18,
-  name: "Organic Chocolatte",
+  name: "Ivan Inozemtsev's project Organic Chocolatte",
   subtitle: "Visual identity",
   imageIds: [ "organic1", "organic2" ]
 }, {
   id: 19,
-  name: "Aimé Leon Dore",
+  name: "Ivan Inozemtsev's project Aimé Leon Dore",
   imageIds: [ "leon1", "leon2", "leon3" ]
 },{
   id: 20,
-  name: "Fatboy the Original",
+  name: "Ivan Inozemtsev's project Fatboy the Original",
   imageIds: [ "fatboy1", "fatboy2", "fatboy3" ]
 },  {
   id: 21,
-  name: "Make Your Work Stand Out",
+  name: "Ivan Inozemtsev's project Make Your Work Stand Out",
   url: "https://www.behance.net/gallery/185380003/Make-your-work-standout",
   imageId: "business"
 }, {
   id: 22,
-  name: "Kids",
+  name: "Ivan Inozemtsev's project Kids",
   imageId: "kids1"
 }, {
   id: 23,
-  name: "Emotions",
+  name: "Ivan Inozemtsev's project Emotions",
   imageId:  "emotions"
 }, {
   id: 24,
-  name: "Symphony of Peaks",
+  name: "Ivan Inozemtsev's project Symphony of Peaks",
   imageId: "peaks"
 }];
 
